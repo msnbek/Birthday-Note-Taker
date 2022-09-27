0143771c97e04f032a435f5557bc4e4b79c125c4
Msnbek <mahmutsenbek@icloud.com>
2022-09-27 13:41:24 +0300

    lastest version

---
+++ BirthdayNoteTaker/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Mahmut Şenbek on 13.09.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var selam: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
